55f8888e45413be6b2b5b80667689b83f0a9df52
Chengqihan <chengqh@vmovier.com>
2020-02-15 13:22:36 +0800

    Initial Commit

---
+++ YYLabel_vs_UILabel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YYLabel_vs_UILabel
+//
+//  Created by chengqihan on 2020/2/15.
+//  Copyright © 2020 chengqihan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ YYLabel_vs_UILabelTests/YYLabel_vs_UILabelTests.swift
@@ -0,0 +1,34 @@
+//
+//  YYLabel_vs_UILabelTests.swift
+//  YYLabel_vs_UILabelTests
+//
+//  Created by chengqihan on 2020/2/15.
+//  Copyright © 2020 chengqihan. All rights reserved.
+//
+
+import XCTest
+@testable import YYLabel_vs_UILabel
+
+class YYLabel_vs_UILabelTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
